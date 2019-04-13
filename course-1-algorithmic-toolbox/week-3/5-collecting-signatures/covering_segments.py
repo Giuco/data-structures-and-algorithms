3f76ff25dc406156a39e861d0f83e8219a829504
Giuco <giucoferrari@gmail.com>
2019-04-13 13:02:17 -0300

    Problem 5 of Week 3 done with stress_test also

---
+++ course-1-algorithmic-toolbox/week-3/5-collecting-signatures/covering_segments.py
@@ -0,0 +1,93 @@
+# Uses python3
+import sys
+from collections import namedtuple
+# from itertools import combinations
+# from tqdm import tqdm
+
+Segment = namedtuple('Segment', 'start end')
+
+
+def is_point_inside_segment(segment, point):
+    # print("Testing")
+    return segment.start <= point <= segment.end
+
+
+def is_segment_covered(segment, list_of_points):
+    covered = False
+
+    for point in list_of_points:
+        if is_point_inside_segment(segment, point):
+            covered = True
+            break
+
+    return covered
+
+
+def is_combination_covering_all_segments(segments, list_of_points):
+    covered = True
+
+    for segment in segments:
+        if not is_segment_covered(segment, list_of_points):
+            covered = False
+            break
+
+    return covered
+
+
+def optimal_points_naive(segments):
+    min_point = 1
+    max_point = max([x.end for x in segments])
+
+    all_possible_points = range(min_point, max_point+1)
+
+    right_combination = None
+
+    for number_of_points in all_possible_points:
+        all_possible_combinations = combinations(all_possible_points, number_of_points)
+
+        for possible_combination in iter(reversed(list(all_possible_combinations))):
+            if is_combination_covering_all_segments(segments, possible_combination):
+                right_combination = possible_combination
+                break
+
+        if right_combination:
+            break
+
+    return right_combination
+
+
+def optimal_points_efficient(segments):
+    segments = iter(sorted(segments, key=lambda x: x.end))
+    segment = next(segments)
+    maximum_point = segment.end
+    right_combination = list()
+
+    while True:
+        try:
+            if not is_point_inside_segment(segment, maximum_point):
+                right_combination.append(maximum_point)
+                maximum_point = segment.end
+            else:
+                segment = next(segments)
+        except StopIteration:
+            break
+
+    right_combination.append(maximum_point)
+
+    return right_combination
+
+
+if __name__ == '__main__':
+    input_data = sys.stdin.read()
+    n, *data = map(int, input_data.split())
+    input_segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+
+    # input_data = [(1, 3), (2, 5), (3, 6)]
+    # input_data = [(4, 7), (1, 3), (2, 5), (5, 6)]
+    # input_segments = [Segment(start, end) for start, end in input_data]
+
+    output_points = optimal_points_efficient(input_segments)
+
+    print(len(output_points))
+    for p in output_points:
+        print(p, end=' ')
---
+++ course-1-algorithmic-toolbox/week-3/5-collecting-signatures/stress_test.py
@@ -0,0 +1,42 @@
+from covering_segments import optimal_points_naive, optimal_points_efficient, Segment
+from random import randint
+from time import time
+from statistics import mean
+
+NUMBER_TESTS = 100
+MAX_NUMBER_SEGMENTS = 30
+MAX_POSSIBLE_POINT = 30
+
+
+def main():
+    for _ in range(NUMBER_TESTS):
+        n = randint(1, MAX_NUMBER_SEGMENTS)
+
+        input_segments = list()
+        for _ in range(n):
+            start = randint(1, MAX_POSSIBLE_POINT - 1)
+            end = randint(start, MAX_POSSIBLE_POINT)
+
+            input_segments.append(Segment(start, end))
+
+        eff_time, nai_time = list(), list()
+
+        print("-")
+
+        st = time()
+        eff_result = optimal_points_efficient(input_segments)
+        eff_time.append(time() - st)
+
+        st = time()
+        nai_result = optimal_points_naive(input_segments)
+        nai_time.append(time() - st)
+
+        if len(nai_result) != len(eff_result):
+            print("Naive Result: {} \t\t Efficient Result: {}".format(nai_result, eff_result))
+            break
+
+    print("Naive Avg. Time: {:.10f} | Efficient Avg. Time: {:.10f}".format(mean(nai_time), mean(eff_time)))
+
+
+if __name__ == '__main__':
+    main()
