38bc63555ed7944aa464877545ed17c70c5c5164
Giuco <giucoferrari@gmail.com>
2019-04-21 15:56:01 -0300

    Done Course 1 - Week 4 - Problem 4

---
+++ course-1-algorithmic-toolbox/week-4/4-number-of-inversions/test_inversions.py
@@ -0,0 +1,28 @@
+from inversions import get_number_of_inversions
+
+
+def test_get_number_of_inversion_1():
+    input_list = [2, 3, 9, 2, 9]
+
+    assert get_number_of_inversions(input_list) == 2
+
+
+def test_get_number_of_inversions_2():
+    input_list = [1, 20, 6, 4, 5]
+
+    assert get_number_of_inversions(input_list) == 5
+
+
+def test_get_number_of_inversions_3():
+    input_list = [1, 2, 3, 4, 5]
+
+    assert get_number_of_inversions(input_list) == 0
+
+
+def test_get_number_of_inversion_4():
+    input_list = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    len_list = len(input_list)
+    answer = len_list*((len_list - 1)/2)
+
+    assert get_number_of_inversions(input_list) == answer
+
