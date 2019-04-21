971fdd86e62c3f37e8a3ad9f4504bc88fbbddeb8
Giuco <giucoferrari@gmail.com>
2019-04-21 19:34:33 -0300

    Reformatted Course 1 - Week 4 - Problem 6

--- course-1-algorithmic-toolbox/week-4/6-closest-points/closest.py
+++ course-1-algorithmic-toolbox/week-4/6-closest-points/closest.py
@@ -1,15 +1,17 @@
-#Uses python3
+# Uses python3
 import sys
-import math
+
 
 def minimum_distance(x, y):
-    #write your code here
-    return 10 ** 18
+    raise NotImplementedError
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    input_data = sys.stdin.read()
+    data = list(map(int, input_data.split()))
+
     n = data[0]
     x = data[1::2]
     y = data[2::2]
+
     print("{0:.9f}".format(minimum_distance(x, y)))
