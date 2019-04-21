26425c4f4bd885d2e344715174135c3e12f6a426
Giuco <giucoferrari@gmail.com>
2019-04-21 19:33:10 -0300

    Reformatted Course 1 - Week 4 - Problem 5

--- course-1-algorithmic-toolbox/week-4/5-organizing-a-lottery/points_and_segments.py
+++ course-1-algorithmic-toolbox/week-4/5-organizing-a-lottery/points_and_segments.py
@@ -1,12 +1,14 @@
 # Uses python3
 import sys
 
-def fast_count_segments(starts, ends, points):
+
+def count_segments_efficient(starts, ends, points):
     cnt = [0] * len(points)
-    #write your code here
+    # write your code here
     return cnt
 
-def naive_count_segments(starts, ends, points):
+
+def count_segments_naive(starts, ends, points):
     cnt = [0] * len(points)
     for i in range(len(points)):
         for j in range(len(starts)):
@@ -14,15 +16,18 @@ def naive_count_segments(starts, ends, points):
                 cnt[i] += 1
     return cnt
 
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    input_data = sys.stdin.read()
+    data = list(map(int, input_data.split()))
+
     n = data[0]
     m = data[1]
-    starts = data[2:2 * n + 2:2]
-    ends   = data[3:2 * n + 2:2]
-    points = data[2 * n + 2:]
-    #use fast_count_segments
-    cnt = naive_count_segments(starts, ends, points)
-    for x in cnt:
+
+    starts_input = data[2:2 * n + 2:2]
+    ends_input = data[3:2 * n + 2:2]
+    points_input = data[2 * n + 2:]
+
+    count_output = count_segments_naive(starts_input, ends_input, points_input)
+    for x in count_output:
         print(x, end=' ')
