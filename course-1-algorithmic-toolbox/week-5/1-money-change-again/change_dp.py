3caeadbf34d25aed69835dc8040b6158e30fe601
Giuco <giucoferrari@gmail.com>
2019-04-28 20:51:20 -0300

    Done Problem 1 - Week 5 - Course 1

--- course-1-algorithmic-toolbox/week-5/1-money-change-again/change_dp.py
+++ course-1-algorithmic-toolbox/week-5/1-money-change-again/change_dp.py
@@ -1,21 +1,32 @@
 # Uses python3
 import sys
+from math import inf
 
 
-COIN_DENOMINATIONS = [4, 3, 1]
-
-
-def get_change(m):
+def get_change(total_amount_of_money, coin_denominations):
     """
-    >>> get_change(2)
+    >>> get_change(2, [4, 3, 1])
     2
-    >>> get_change(34)
+
+    >>> get_change(6, [4, 3, 1])
+    2
+
+    >>> get_change(34, [3, 4, 1])
     9
     """
+    n_coins = [inf] * (total_amount_of_money+1)
 
-    return m // 4
+    for money_subset in range(1, total_amount_of_money+1):
+        for coin in coin_denominations:
+            if coin == money_subset:
+                n_coins[money_subset] = 1
+            elif coin < money_subset:
+                n_coins[money_subset] = min(n_coins[money_subset - coin] + 1, n_coins[money_subset])
+
+    return n_coins[total_amount_of_money]
 
 
 if __name__ == '__main__':
     m_input = int(sys.stdin.read())
-    print(get_change(m_input))
+    coin_denominations_input = [4, 3, 1]
+    print(get_change(m_input, coin_denominations_input))
