a77d2f101905412b7480d4cbe380031bd0adfd00
Giuco <giucoferrari@gmail.com>
2019-04-29 15:12:24 -0300

    Done Problem 4 - Week 5 - Course 1

--- course-1-algorithmic-toolbox/week-5/4-longest-common-subsequence-of-two-sequences/lcs2.py
+++ course-1-algorithmic-toolbox/week-5/4-longest-common-subsequence-of-two-sequences/lcs2.py
@@ -1,8 +1,9 @@
 # Uses python3
 
 import sys
+from itertools import combinations
 from numbers import Number
-from typing import List, Tuple
+from typing import List
 
 
 def lis(arr: List[Number]) -> List[Number]:
@@ -38,9 +39,80 @@ def reconstruct_lis(arr: List[Number], t: List[int]) -> List[Number]:
     return list(reversed(reconstructed_lis))
 
 
-def lcs2(a, b):
-    # write your code here
-    return min(len(a), len(b))
+def lcs2_naive(a: List[Number], b: List[Number]) -> List[Number]:
+    """
+    >>> len(lcs2_naive([2, 7, 5], [2, 5]))
+    2
+
+    >>> len(lcs2_naive([7], [1, 2, 3, 4]))
+    0
+
+    >>> len(lcs2_naive([2, 7, 8, 3], [5, 2, 8, 7]))
+    2
+    """
+    a = [x for x in a if x in b]
+
+    min_length = min(len(a), len(b))
+
+    for n in range(min_length, 0, -1):
+        a_combinations = combinations(a, n)
+        for a_combination in a_combinations:
+            b_combinations = combinations(b, n)
+            for b_combination in b_combinations:
+                if a_combination == b_combination:
+                    return list(a_combination)
+
+    return []
+
+
+def lcs2_recursive(a: List[Number], b: List[Number]) -> int:
+    """
+    >>> lcs2([2, 7, 5], [2, 5])
+    2
+
+    >>> lcs2([7], [1, 2, 3, 4])
+    0
+
+    >>> lcs2([2, 7, 8, 3], [5, 2, 8, 7])
+    2
+    """
+    m = len(a)
+    n = len(b)
+
+    if m == 0 or n == 0:
+        return 0
+    elif a[m - 1] == b[n - 1]:
+        return 1 + lcs2_recursive(a[:-1], b[:-1])
+    else:
+        return max(lcs2_recursive(a[:-1], b), lcs2_recursive(a, b[:-1]))
+
+
+def lcs2(a: List[Number], b: List[Number]) -> int:
+    """
+    >>> lcs2([2, 7, 5], [2, 5])
+    2
+
+    >>> lcs2([7], [1, 2, 3, 4])
+    0
+
+    >>> lcs2([2, 7, 8, 3], [5, 2, 8, 7])
+    2
+    """
+    m = len(a)
+    n = len(b)
+
+    table = dict()
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                table[i, j] = 0
+            elif a[i - 1] == b[j - 1]:
+                table[i, j] = 1 + table[i - 1, j - 1]
+            else:
+                table[i, j] = max(table[i - 1, j], table[i, j - 1])
+
+    return table[m, n]
 
 
 if __name__ == '__main__':
