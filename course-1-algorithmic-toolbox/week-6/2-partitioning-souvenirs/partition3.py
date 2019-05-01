b0ba2ea66c71e22ca9da75f8a43498f6ade8be9d
Giuco <giucoferrari@gmail.com>
2019-05-01 12:58:33 -0300

    Done Problem 2 - Week 6 - Course 1

--- course-1-algorithmic-toolbox/week-6/2-partitioning-souvenirs/partition3.py
+++ course-1-algorithmic-toolbox/week-6/2-partitioning-souvenirs/partition3.py
@@ -1,15 +1,28 @@
 # Uses python3
-import itertools
 import sys
+from itertools import product
 
 
 def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
+    """
+    >>> partition3([3, 3, 3, 3])
+    0
+
+    >>> partition3([40])
+    0
+
+    >>> partition3([17, 59, 34, 57, 17, 23, 67, 1, 18, 2, 59])
+    1
+
+    >>> partition3([1, 2, 3, 4, 5, 5, 7, 7, 8, 10, 12, 19, 25])
+    1
+    """
+    for c in product(range(3), repeat=len(A)):
         sums = [None] * 3
         for i in range(3):
             sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
+        if sums[0] == sums[1] == sums[2]:
             return 1
 
     return 0
