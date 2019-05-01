eecd44d5bffe1931bb96b70afc1a4bbc7ee278ac
Giuco <giucoferrari@gmail.com>
2019-05-01 14:08:10 -0300

    Done Problem 5 - Week 4 - Course 1

--- course-1-algorithmic-toolbox/week-4/5-organizing-a-lottery/points_and_segments.py
+++ course-1-algorithmic-toolbox/week-4/5-organizing-a-lottery/points_and_segments.py
@@ -2,13 +2,52 @@
 import sys
 
 
-def count_segments_efficient(starts, ends, points):
-    cnt = [0] * len(points)
-    # write your code here
-    return cnt
+def count_segments_efficient(starts, ends, points_list):
+    """
+    >>> count_segments_efficient([2, 0], [3, 5], [1, 6, 11])
+    [1, 0, 0]
+
+    >>> count_segments_efficient([1, -10], [3, 10], [-100, 100, 0])
+    [0, 0, 1]
+
+    >>> count_segments_efficient([3, 0, -3, 7], [2, 5, 2, 10], [1, 6])
+    [2, 0]
+    """
+    cnt = {}
+
+    array = [(start, 0) for start in starts] + \
+            [(end, 2) for end in ends] + \
+            [(point, 1) for point in points_list]
+
+    array = sorted(array)
+
+    charge = 0
+    for p, ob in array:
+        if ob == 0:
+            charge += 1
+        elif ob == 2:
+            charge -= 1
+        else:
+            cnt[p] = charge
+
+    result = list()
+    for p in points_list:
+        result.append(cnt[p])
+
+    return result
 
 
 def count_segments_naive(starts, ends, points):
+    """
+    >>> count_segments_naive([2, 0], [3, 5], [1, 6, 11])
+    [1, 0, 0]
+
+    >>> count_segments_naive([1, -10], [3, 10], [-100, 100, 0])
+    [0, 0, 1]
+
+    >>> count_segments_naive([3, 0, -3, 7], [2, 5, 2, 10], [1, 6])
+    [2, 0]
+    """
     cnt = [0] * len(points)
     for i in range(len(points)):
         for j in range(len(starts)):
@@ -28,6 +67,6 @@ if __name__ == '__main__':
     ends_input = data[3:2 * n + 2:2]
     points_input = data[2 * n + 2:]
 
-    count_output = count_segments_naive(starts_input, ends_input, points_input)
+    count_output = count_segments_efficient(starts_input, ends_input, points_input)
     for x in count_output:
         print(x, end=' ')
