d260cf2df3bf81affcdc2da56dffca9914492cbd
Giuco <giucoferrari@gmail.com>
2019-05-09 19:32:05 -0300

    Work on Problem 3 - Week 6 - Course 1

--- course-1-algorithmic-toolbox/week-6/3-maximum-value-of-an-arithmetic-expression/placing_parentheses.py
+++ course-1-algorithmic-toolbox/week-6/3-maximum-value-of-an-arithmetic-expression/placing_parentheses.py
@@ -1,6 +1,6 @@
 # Uses python3
 from numbers import Number
-from typing import Union
+from typing import Union, List, Tuple
 
 
 def eval_operation(a: Number, b: Number, op: str) -> Number:
@@ -22,8 +22,23 @@ def convert_to_number_if_possible(string: str) -> Union[str, Number]:
         return string.strip()
 
 
-def get_maximum_value(data: str):
-    elements = map(convert_to_number_if_possible, data.split())
+def string_to_ops_and_numbers(data: str) -> Tuple[List[Number], List[str]]:
+    elements = list(map(convert_to_number_if_possible, data.split()))
+    numbers = elements[0::2]
+    operations = elements[1::2]
+
+    return numbers, operations
+
+
+def get_maximum_value(data: str) -> Number:
+    numbers, operations = string_to_ops_and_numbers(data)
+
+    min_table = [[0] * len(numbers)] * len(numbers)
+    max_table = [[0] * len(numbers)] * len(numbers)
+
+    for i in range(len(numbers)):
+        min_table[i][i] = numbers[i]
+        max_table[i][i] = numbers[i]
 
 
 if __name__ == "__main__":
