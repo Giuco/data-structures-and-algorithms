eb5205a288bb7530513d43e46a294808500de96a
Giuco <giucoferrari@gmail.com>
2019-05-17 16:33:15 -0300

    Done Course 2 - Week 1 - Problem 2

--- course-2-data-structures/week-1/2-tree-height/tree_height.py
+++ course-2-data-structures/week-1/2-tree-height/tree_height.py
@@ -1,11 +1,28 @@
 # python3
 
-import sys
-import threading
+from typing import List
 
 
-def compute_height(n, parents):
-    # Replace this code with a faster implementation
+class TreeNode:
+    def __init__(self, value: int):
+        self.value = value
+        self.children = []
+
+    def add_children(self, value):
+        self.children.append(value)
+
+
+def compute_height_naive(n: int, parents: List[int]) -> int:
+    """
+    >>> compute_height_naive(5, [4, -1, 4, 1, 1])
+    3
+
+    >>> compute_height_naive(5, [-1, 0, 4, 0, 3])
+    4
+
+    >>> compute_height_naive(10, [9, 7, 5, 5, 2, 9, 9, 9, 2, -1])
+    4
+    """
     max_height = 0
     for vertex in range(n):
         height = 0
@@ -17,15 +34,49 @@ def compute_height(n, parents):
     return max_height
 
 
+def compute_height(n: int, parents: List[int]) -> int:
+    """
+    >>> compute_height(5, [4, -1, 4, 1, 1])
+    3
+
+    >>> compute_height(5, [-1, 0, 4, 0, 3])
+    4
+
+    >>> compute_height(10, [9, 7, 5, 5, 2, 9, 9, 9, 2, -1])
+    4
+    """
+    nodes = [TreeNode(value=i) for i in range(n)]
+    root = None
+
+    for children_index in range(n):
+        parent_index = parents[children_index]
+        if parent_index == -1:
+            root = children_index
+        else:
+            nodes[parent_index].add_children(nodes[children_index])
+
+    if root is None:
+        raise ValueError("Did not found the root node")
+
+    tree_stack = [nodes[root]]
+    tree_size = 0
+
+    while tree_stack:
+        tree_size += 1
+        temp_tree_stack = []
+        for parent in tree_stack:
+            for child in parent.children:
+                temp_tree_stack.append(child)
+        tree_stack = temp_tree_stack
+
+    return tree_size
+
+
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
     print(compute_height(n, parents))
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10 ** 7)  # max depth of recursion
-threading.stack_size(2 ** 27)  # new thread will get stack of such size
-threading.Thread(target=main).start()
+if __name__ == "__main__":
+    main()
